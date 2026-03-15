9148d341c548f3bb63617a5a06c2a3b9ffbea0b7
Prashant <143874787+PRASHJANNUZ@users.noreply.github.com>
2026-03-16 00:42:12 +0530

    Change email and password in config.py
    Updated email credentials in config.py.

---
+++ config.py
@@ -0,0 +1,26 @@
+EMAIL = "prashantjannu227@gmail.com"
+PASSWORD = "prashant302"
+TO_EMAIL = "prashantjannu227@gmail.com"
+
+KEYWORDS = [
+    "java developer",
+    "spring boot",
+    "backend developer",
+    "software engineer",
+]
+
+SKILLS = [
+    "java",
+    "spring",
+    "springboot",
+    "microservices",
+    "rest api"
+]
+
+LOCATIONS = [
+    "pune",
+    "hyderabad"
+]
+
+MIN_EXP = 1
+MAX_EXP = 4
